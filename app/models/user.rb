5750f7c054da696a6c13d59ed2c485e32997c1b9
依田尚悟 <>
2021-04-28 10:29:41 +0900

    no message

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #  index_users_on_uid_and_provider      (uid,provider) UNIQUE
 #
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -41,5 +41,4 @@ class User < ActiveRecord::Base
   has_many :artiles, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
-
 end
