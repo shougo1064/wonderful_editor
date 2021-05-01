6c34e6c9ae1d1c9f8394135639a02c2bd79e554a
依田尚悟 <>
2021-05-01 17:08:34 +0900

    新規登録 API の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
   has_many :articles, dependent: :destroy
