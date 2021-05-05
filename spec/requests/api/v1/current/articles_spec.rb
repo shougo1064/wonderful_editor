8e7e9e98bc597824e8fb95503af5b4b8a1780d47
依田尚悟 <>
2021-05-05 13:11:22 +0900

    フロントエンドとの繋ぎこみ part.3

--- spec/requests/api/v1/current/articles_spec.rb
+++ spec/requests/api/v1/current/articles_spec.rb
@@ -1,17 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Api::V1::Current::Articles", type: :request do
   describe "GET /api/v1/current/articles" do
     subject { get(api_v1_current_articles_path, headers: headers) }
+
     let(:headers) { current_user.create_new_auth_token }
     let(:current_user) { create(:user) }
-    let!(:article1) { create(:article, :published, updated_at:1.days.ago, user: current_user) }
+    let!(:article1) { create(:article, :published, updated_at: 1.days.ago, user: current_user) }
     let!(:article2) { create(:article, :published, user: current_user) }
     let!(:article3) { create(:article, :draft, user: current_user) }
-    let!(:article4) { create(:article, :published)}
-    fit "自分が書いた公開状態の記事の一覧が取得できる（更新順）" do
+    let!(:article4) { create(:article, :published) }
+    it "自分が書いた公開状態の記事の一覧が取得できる（更新順）" do
       subject
-       res = JSON.parse(response.body)
+      res = JSON.parse(response.body)
       expect(res.length).to eq 2
       expect(res.map {|d| d["id"] }).to eq [article2.id, article1.id]
       expect(res[0]["status"]).to eq "published"
